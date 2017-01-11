b71ef582cafccf108130b5e8db3188254913ed13
Kaio Magalhães <me@kaiomagalhaes.com>
2017-01-11 03:05:58 -0200

    update dockerfile and deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,15 +3,21 @@
 echo "inserting the image version in docker-compose template"
 bash -c 'sed -i "s/kaiomagalhaes\/rails-base-project/kaiomagalhaes\/rails-base-project:$VERSION/" config/docker-compose.yml.template'
 
-echo "copying docker-compose"
-scp config/docker-compose.yml.template $DEPLOY_USER@$DEPLOY_HOST:/opt/rails-base-project/config/docker-compose.yml.backend
+echo "creating projects folder if it doesn't exist"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'mkdir -p projects/rails-base-project/config'
 
+echo "copying docker-compose"
+scp config/docker-compose.yml.template $DEPLOY_USER@$DEPLOY_HOST:projects/rails-base-project/config/docker-compose.yml.backend
 
 echo "pulling latest version of the code"
-ssh $DEPLOY_USER@$DEPLOY_HOST "docker-compose -f /opt/rails-base-project/config/docker-compose.yml.backend pull rails-base-project"
+ssh $DEPLOY_USER@$DEPLOY_HOST "docker-compose -f projects/rails-base-project/config/docker-compose.yml.backend pull rails-base-project"
 
 echo "starting the new version"
-ssh $DEPLOY_USER@$DEPLOY_HOST 'docker-compose -f /opt/rails-base-project/config/docker-compose.yml.backend up -d rails-base-project'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker-compose -f projects/rails-base-project/config/docker-compose.yml.backend up -d rails-base-project'
+
+echo "creating network"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'if [ $(docker network ls | grep kaiomagalhaes | wc -l) -gt 0 ]; then echo "network already exists"; else docker network create kaiomagalhaes ; fi'
+
 
 echo "removing old and unsed images"
 ssh $DEPLOY_USER@$DEPLOY_HOST "docker images --filter 'dangling=true' --format '{{.ID}}' | xargs docker rmi"
