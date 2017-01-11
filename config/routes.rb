572675f19dc9ae7fd8b023447dda6cba7bcf917a
Kaio Magalhães <me@kaiomagalhaes.com>
2017-01-11 15:26:37 -0200

    add users test model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  root 'users#index'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
